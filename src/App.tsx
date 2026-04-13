16106334af8658bf20b3fa11c257cf94bff923dc
Massamba-commits <paparafinha20@gmail.com>
2026-04-13 11:54:54 +0000

    premier commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline, text-center mt-10 text-red-800">
+        Hello world!
+      </h1>
+    </div>
+  )
+
+}
+
+export default App
